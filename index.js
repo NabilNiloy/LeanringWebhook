33cbb9947d7f4585a2af2706536522af8d8d8bd1
NabilNiloy <niloy2001@yahoo.com>
2022-09-07 12:41:17 +0100

    updated index.js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-//payload will be recieved in this host
+
 app.post('/', (req, res)  => {
     console.log(req.body); //printing the received payload
     res.status(200).end;  //responding to the organization API
