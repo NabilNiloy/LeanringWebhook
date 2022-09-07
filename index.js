f102268dd401ce34989b93f3c94e4c8753fecb89
NabilNiloy <niloy2001@yahoo.com>
2022-09-07 12:34:14 +0100

    updated index.js file

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res)  => {
+    console.log(req.body); //printing the received payload
+    res.status(200).end;  //responding to the organization API
+})
+
+const PORT = 4567;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port no ${PORT}`);
+});
+
